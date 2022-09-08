fa3643fb3bdbb5074cfa20acec0cd75bdae2dfeb
oyayafelix <oyayafelix@gmail.com>
2022-09-08 12:45:27 +0300

    commit tree 994af5291dcf792dc3b7d7b7818a7d01f5567569

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_19_113221) do
+ActiveRecord::Schema.define(version: 2022_09_08_094413) do
 
   create_table "games", force: :cascade do |t|
     t.string "title"
